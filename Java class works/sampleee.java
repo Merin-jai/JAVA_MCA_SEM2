e9518d36ca5dba5b2fa54aa6579995d137a3b5ec
Merin-jai <145257949+Merin-jai@users.noreply.github.com>
2025-04-12 23:54:40 +0530

    pgm

---
+++ Java class works/sampleee.java
@@ -0,0 +1,19 @@
+class sampleee
+{
+    public static void main(String[] args) {
+        try{
+            Float f1=new Float("3.0");
+        int x=f1.intValue();
+        byte b=f1.byteValue();
+        double d=f1.doubleValue();
+        System.out.println(x+b+d);
+        char c='a';
+        if()
+        }
+        catch(NumberFormatException e)
+        {
+            System.out.println("Bad no");
+        }
+
+    }
+}
