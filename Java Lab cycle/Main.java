d9439edfdf96526b2c91a0f6a1a226aef72bfd79
Merin-jai <145257949+Merin-jai@users.noreply.github.com>
2025-04-17 23:52:12 +0530

    pgm

---
+++ Java Lab cycle/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Employee {
+    public void display() {
+        System.out.println("Name of class is Employee.");
+    }
+
+    public void calcSalary() {
+        System.out.println("Salary of employee is 10000.");
+    }
+}
+
+class Engineer extends Employee {
+    @Override
+    public void calcSalary() {
+        System.out.println("Salary of employee is 20000.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Engineer(); // Polymorphism: Employee reference, Engineer object
+        emp.display(); // Calls display method of Employee class
+        emp.calcSalary(); // Calls calcSalary method of Engineer class
+    }
+}
