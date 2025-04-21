8a3cbcdfd900dae2ca40c17e3c322fa01294cac0
Merin-jai <145257949+Merin-jai@users.noreply.github.com>
2025-04-21 23:43:06 +0530

    pgm

---
+++ Java Lab cycle/procedure.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+import java.util.Scanner;  
+public class procedure
+{  
+	public static void main(String[] args) throws Exception
+	{   
+        int id;String n;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter id");
+        id=s.nextInt();
+        System.out.println("Enter name");s.nextLine();
+        n=s.nextLine();
+  		System.out.println("success");  
+	}  
+}
